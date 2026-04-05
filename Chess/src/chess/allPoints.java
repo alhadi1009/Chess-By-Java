8700ab8aeaa3e88b74a7a1cc0760279d61ce98e4
Al Hadi <alhadi1009.bd@gmail.com>
2026-04-06 02:36:53 +0600

    first commmit

--- Chess/src/chess/allPoints.java
+++ Chess/src/chess/allPoints.java
@@ -11,6 +11,42 @@ import java.awt.Point;
  * @author Al Hadi
  */
 public class allPoints {
-    public static positions[] greenPath = new Point[32];
+    public static Point[] Positions = new Point[32];
+    static{
+        Positions[0]=new Point(0,480);
+        Positions[1]=new Point(80,480);
+        Positions[2]=new Point(160,480);
+        Positions[3]=new Point(240,480);
+        Positions[4]=new Point(320,480);
+        Positions[5]=new Point(400,480);
+        Positions[6]=new Point(480,480);
+        Positions[7]=new Point(560,480);
+        Positions[8]=new Point(0,560);
+        Positions[9]=new Point(80,560);
+        Positions[10]=new Point(160,560);
+        Positions[11]=new Point(240,560);
+        Positions[12]=new Point(320,560);
+        Positions[13]=new Point(400,560);
+        Positions[14]=new Point(480,560);
+        Positions[15]=new Point(560,560);
+        Positions[16]=new Point(0,80);
+        Positions[17]=new Point(80,80);
+        Positions[18]=new Point(160,80);
+        Positions[19]=new Point(240,80);
+        Positions[20]=new Point(320,80);
+        Positions[21]=new Point(400,80);
+        Positions[22]=new Point(480,80);
+        Positions[23]=new Point(560,80);
+        Positions[24]=new Point(0,0);
+        Positions[25]=new Point(80,0);
+        Positions[26]=new Point(160,0);
+        Positions[27]=new Point(240,0);
+        Positions[28]=new Point(320,0);
+        Positions[29]=new Point(400,0);
+        Positions[30]=new Point(480,0);
+        Positions[31]=new Point(560,0);
+        
+        
+    }
     
 }
